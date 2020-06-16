17fe75c4bca6f4ee5b581ee9d230b53172a259c8
Ingo <ingovanduijn@gmail.com>
2020-06-16 15:05:55 +0200

    Add entity insert logic (and remove pycache)

--- jigs/routes.py
+++ jigs/routes.py
@@ -1,5 +1,8 @@
+import json
+
 from flask import (Blueprint,
                    request,
+                   abort,
                    current_app as app)
 
 from google.cloud import datastore
@@ -9,10 +12,14 @@ jig_bp = Blueprint('jig_bp', __name__)
 
 @jig_bp.route('/', methods=['POST'])
 def gather():
-    player_id = request.form.get('player_id', '')
-    puzzle_id = request.form.get('puzzle_id', '')
-    entry_key = app.db.key("log", puzzle_id)
-    entry = datastore.Entity(key=entry_key)
-    entry[player_id] = player_id
+    if not (data := request.get_json(silent=True)):
+        abort(415)
+    player_key = app.db.key("player", data["playerSecret"])
+    player = datastore.Entity(key=player_key)
+    puzzle_key = app.db.key("puzzle", data["puzzleID"], parent=player_key)
+    entry = datastore.Entity(key=puzzle_key)
+    for key, value in data["events"].items():
+        entry[key] = json.dumps(value)
+    app.db.put(player)
     app.db.put(entry)
-    return {player_id: player_id, puzzle_id: puzzle_id}
+    return data
