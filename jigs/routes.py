4c743a495003c5a1edca827ca4bb37f6e98ca9aa
Ingo <ingovanduijn@gmail.com>
2020-06-16 15:20:06 +0200

    Add sanity checks for POST data

--- jigs/routes.py
+++ jigs/routes.py
@@ -14,12 +14,20 @@ jig_bp = Blueprint('jig_bp', __name__)
 def gather():
     if not (data := request.get_json(silent=True)):
         abort(415)
-    player_key = app.db.key("player", data["playerSecret"])
+    if not (playerSecret := data.get("playerSecret")):
+        abort(400)
+    if not (puzzleID := data.get("puzzleID")):
+        abort(400)
+    if not (events := data.get("events")):
+        abort(400)
+    if not isinstance(events, dict):
+        abort(400)
+    player_key = app.db.key("player", playerSecret)
     player = datastore.Entity(key=player_key)
-    puzzle_key = app.db.key("puzzle", data["puzzleID"], parent=player_key)
+    puzzle_key = app.db.key("puzzle", puzzleID, parent=player_key)
     entry = datastore.Entity(key=puzzle_key)
-    for key, value in data["events"].items():
+    for key, value in events.items():
         entry[key] = json.dumps(value)
     app.db.put(player)
     app.db.put(entry)
-    return data
+    return app.response_class(status=200)
